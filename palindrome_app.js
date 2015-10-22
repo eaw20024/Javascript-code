72fa625b1817954762d32d9aaefd83b7c73cb9ee
eaw5117 <eric.washington.me@gmail.com>
2015-10-22 15:51:41 +0000

    Refactored Palindrome App

--- palindrome_app.js
+++ palindrome_app.js
@@ -1,54 +1,19 @@
-<head>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<title>Check Palindrome with JavaScript Program</title>
-	<style>
-		input{
-			padding: 10px;			
-		}
-	</style>
-	<script>
-		
-		function myFunction()
-		{
-			//get the value from textbox
-			var str = document.getElementById('txtbox').value;
-			
-			//call checkPalindrome() and pass str
-			var result = checkPalindrome(str);
-			alert('The Entered String "'+str +'" is "'+result+'"');
-		}
-		
-		function checkPalindrome(str)
-		{
-			var orignalStr;
-			 
-			//lowercase the string 
-			str = str.toLowerCase();
-			
-			//store the str in orignalStr for future use
-			orignalStr = str;
-			
-			//reverse the entered string
-			str = str.split(''); //split the entered string 
-			str = str.reverse(); //reverse the order 
-			str = str.join(''); //then join the reverse order array values
-			
-			var reverseStr = str;
-			
-			//finally check both the Original string stored in orignalStr
-			//and reversed to find the palindrom			
-			if(orignalStr == reverseStr){
-				return 'Palindrome'; // return "Palindrome" if true
-			}else{
-				return 'Not Palindrome';
-			}
-		}
-	</script>
-</head>
-<body>
-	<form action="http://www.ericwashington.me" method="get">
-		<input type="text" id="txtbox" placeholder="Enter a 'String'" />
-		<input type="button" onclick="myFunction()" value="Check Palindrome" />
-	</form>
-</body>
-</html>
+function palindrome(str) {
+  //Use regular expressions to replace any uppercase letter into a lowercase letter
+  var normalizedStr = str.replace(/[\W_]/g, '').toLowerCase();
+  //Check to see if the string is the same as the reversed string using split method
+  var reverseStr = normalizedStr.split('').reverse().join('');
+  //Reverse the string and join it back together
+  return normalizedStr === reverseStr;
+}
+
+palindrome("eye");
+palindrome("race car");
+palindrome("not a palindrome");
+palindrome("A man, a plan, a canal. Panama");
+palindrome("never odd or even");
+palindrome("nope");
+palindrome("almostomla");
+palindrome("My age is 0, 0 si ega ym.");
+palindrome("1 eye for of 1 eye.");
+palindrome("0_0 (: /-\ :) 0-0");
