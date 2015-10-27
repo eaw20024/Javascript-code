ad1aaec26eb9faaf3210e6b90277cda986603e4a
eaw5117 <eric.washington.me@gmail.com>
2015-10-27 18:40:32 +0000

    Created a program that repeats a string and turns it into a new string

---
+++ repeatString.js
@@ -0,0 +1,14 @@
+function repeat(string, times) {
+  //Created a string to stor the variable
+  var newstr = [];
+  //Create a loop to repeat the code as many times as needed
+  for (var i = 0; i < num; i++) {
+    newstr.push(str);
+  };
+  //Make the variable created store the current value and append the word to it
+  return newstr.join('');
+}
+
+repeat("*", 3);
+repeat("abc", 3);
+repeat("abc", -2);
